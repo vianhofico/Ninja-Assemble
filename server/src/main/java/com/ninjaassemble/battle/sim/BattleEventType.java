fdd771f2e90d6ccad331bbf0aaa0bbfcb146c60c
vianhofico <vianhofico@gmail.com>
2026-08-17 21:09:58 +0700

    M47 extend battle event protocol for realtime

--- server/src/main/java/com/ninjaassemble/battle/sim/BattleEventType.java
+++ server/src/main/java/com/ninjaassemble/battle/sim/BattleEventType.java
@@ -3,6 +3,9 @@ package com.ninjaassemble.battle.sim;
 public enum BattleEventType {
     BATTLE_START,
     ROUND_START,
+    ACTION_READY,
+    CAST_START,
+    CAST_COMPLETE,
     PASSIVE_TRIGGER,
     ATTACK,
     DAMAGE,
@@ -12,6 +15,7 @@ public enum BattleEventType {
     ENERGY,
     STATUS_APPLIED,
     STATUS_TICK,
+    STATUS_EXPIRED,
     STATUS_CLEANSED,
     REVIVE,
     TURN_SKIPPED,
