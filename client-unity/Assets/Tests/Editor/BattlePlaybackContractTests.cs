787c7bcca8075a535909ba3399d987b458d37f67
vianhofico <vianhofico@gmail.com>
2026-08-18 18:00:40 +0700

    M54 add Unity playback contract tests

---
+++ client-unity/Assets/Tests/Editor/BattlePlaybackContractTests.cs
@@ -0,0 +1,63 @@
+using System;
+using NinjaAssemble.Presentation;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace NinjaAssemble.Tests.Editor
+{
+    public sealed class BattlePlaybackContractTests
+    {
+        private GameObject root;
+        private BattleTimelinePlayer timeline;
+
+        [SetUp]
+        public void SetUp()
+        {
+            root = new GameObject("BattleTimelineTestRoot", typeof(RectTransform));
+            timeline = root.AddComponent<BattleTimelinePlayer>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (root != null) UnityEngine.Object.DestroyImmediate(root);
+        }
+
+        [Test]
+        public void PlaybackSpeed_AcceptsOnlyOneTwoAndFour()
+        {
+            timeline.SetPlaybackSpeed(1);
+            Assert.AreEqual(1, timeline.PlaybackSpeed);
+
+            timeline.SetPlaybackSpeed(2);
+            Assert.AreEqual(2, timeline.PlaybackSpeed);
+
+            timeline.SetPlaybackSpeed(4);
+            Assert.AreEqual(4, timeline.PlaybackSpeed);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => timeline.SetPlaybackSpeed(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => timeline.SetPlaybackSpeed(3));
+            Assert.Throws<ArgumentOutOfRangeException>(() => timeline.SetPlaybackSpeed(5));
+        }
+
+        [Test]
+        public void PauseState_IsExplicitAndReversible()
+        {
+            Assert.IsFalse(timeline.IsPaused);
+            timeline.SetPaused(true);
+            Assert.IsTrue(timeline.IsPaused);
+            timeline.SetPaused(false);
+            Assert.IsFalse(timeline.IsPaused);
+        }
+
+        [Test]
+        public void NewPlayback_ResetsPauseAndTimestamp()
+        {
+            timeline.SetPaused(true);
+            timeline.Play(Array.Empty<BattlePresentationEvent>());
+
+            Assert.IsFalse(timeline.IsPaused);
+            Assert.AreEqual(0L, timeline.CurrentTimestampMs);
+        }
+    }
+}
